31ee72009bd1b7745a22a42ad2c19c40b5e2db7a
Vijaykumarasamy <vijaykumarasamyvkps@gmail.com>
2026-04-26 19:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (47.58%) - LeetSync

---
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int n) {
+        // int s=0;
+        // for(int i=0;i<n;i++){
+        //     int m=(2*i)+1;
+        //     s+=Math.abs(n-m);
+        // }
+        // return s/2;
+        return (n*n)/4;
+    }
+}

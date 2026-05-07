995be0b858c213d0fe314da239a1c18a931a207a
Vijaykumarasamy <vijaykumarasamyvkps@gmail.com>
2026-05-07 18:09:24 +0530

    Time: 3 ms (96.61%) | Memory: 47.2 MB (59.16%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int a=0,b=0;
+        for(char c:s.toCharArray()){
+            if(c=='a'){
+                a++;
+            }else{
+                b++;
+            }
+        }
+        return Math.abs(a-b);
+    }
+}

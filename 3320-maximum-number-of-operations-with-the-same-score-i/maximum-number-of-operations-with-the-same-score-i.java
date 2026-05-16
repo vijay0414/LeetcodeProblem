db87796840e3c4fcfa2405041fa7481427eae040
Vijaykumarasamy <vijaykumarasamyvkps@gmail.com>
2026-05-16 18:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (74.21%) - LeetSync

---
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxOperations(int[] nums) {
+        int c=1;
+        int sum=nums[0]+nums[1];
+       for(int i=2;i<nums.length-1;i+=2){
+        if(nums[i]+nums[i+1]==sum){
+            c++;
+        }else{
+            break;
+        }
+       }
+        return c;
+    }
+}

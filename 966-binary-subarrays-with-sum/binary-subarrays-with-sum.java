7c42ebd7f55cb48216d0c7141257783aa6e17caf
Vijaykumarasamy <vijaykumarasamyvkps@gmail.com>
2026-06-11 16:09:12 +0530

    Time: 22 ms (40.90%) | Memory: 52.9 MB (8.00%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,15 +1,41 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==goal){
-                    c++;
-                }
+        // int c=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         if(sum==goal){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+
+        // int sum=0,l=0,count=0;
+        // for(int r=0;r<nums.length;r++){
+        //     sum+=nums[r];
+        //     while(sum>goal){
+        //         sum-=nums[l];
+        //         l++;
+        //     }
+        //     if(sum==goal){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        HashMap<Integer,Integer> m=new HashMap<>();
+        int prefsum=0;
+        int count = 0;
+        m.put(0,1);
+        for(int i:nums){
+            prefsum+=i;
+            if(m.containsKey(prefsum-goal)){
+                count+=m.get(prefsum-goal);
             }
+            m.put(prefsum , m.getOrDefault(prefsum,0)+1);
         }
-        return c;
+        return count;
     }
 }

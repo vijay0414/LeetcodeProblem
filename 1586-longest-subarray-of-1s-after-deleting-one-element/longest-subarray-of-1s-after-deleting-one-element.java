2bd975216c633f04cc13541b5217c3c07b7d59b2
Vijaykumarasamy <vijaykumarasamyvkps@gmail.com>
2026-07-03 12:00:43 +0530

    Time: 3 ms (88.16%) | Memory: 65.3 MB (67.01%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -3,21 +3,17 @@ class Solution {
         int l = 0,max = 0,c=0;
         List<Integer> list = new ArrayList<>();
         for(int r = 0;r < nums.length;r++){
-            if(nums[r] != 0){
-                list.add(nums[r]);
-            }else{
-                c++;
-            }
+            if(nums[r] == 0) c++;
             
             while(c > 1){
                 if(nums[l] == 0){
                     c--;
                 }
-                list.clear();
+                //list.clear();
                 l++;
             }
-            max = Math.max(max,r - l + 1);
+            max = Math.max(max,r - l);
         }
-        return max - 1;
+        return max;
     }
 }

990043748f44c012c43d3decda955dcd1f243999
Vijaykumar <vijaykumarasamyvkps@gmail.com>
2026-08-05 15:33:17 +0530

    Time: 4 ms (66.00%) | Memory: 94.3 MB (80.35%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,14 +10,44 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        String str = "";
-        StringBuilder str1 = new StringBuilder();
-        while(head != null){
-            str += String.valueOf(head.val);
-            str1.append(String.valueOf(head.val));
-            head = head.next;
+        // String str = "";
+        // StringBuilder str1 = new StringBuilder();
+        // while(head != null){
+        //     str += String.valueOf(head.val);
+        //     str1.append(String.valueOf(head.val));
+        //     head = head.next;
+        // }
+        // String last = str1.reverse().toString();
+        // return str.equals(last);
+
+        //slow and Fast pointer
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        String last = str1.reverse().toString();
-        return str.equals(last);
+
+        ListNode ri = reverse(slow);
+        ListNode le = head;
+
+        while(ri != null){
+            if(ri.val != le.val) return false;
+            ri = ri.next;
+            le = le.next;
+        }
+        return true;
+    }
+
+    //Reverse
+    public static ListNode reverse (ListNode head){
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
